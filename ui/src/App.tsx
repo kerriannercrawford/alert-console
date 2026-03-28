4d9506d9a9b0f6015ce6dadd7ecff7e632e90a9e
kerriannercrawford <kerriannercrawford+github@gmail.com>
2026-03-28 15:40:40 -0400

    scaffold react app

---
+++ ui/src/App.tsx
@@ -0,0 +1,8 @@
+export default function App() {
+    return (
+        <div className="App">
+            <h1>Welcome to the UI!</h1>
+            <p>This is a placeholder.</p>
+        </div>
+    )
+}
